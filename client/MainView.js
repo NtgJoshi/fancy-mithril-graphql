ce97972262eacb0eccd18865fdc3b7d51cb8054d
Josua <josua.brinnig-ext@my-rio.de>
2019-06-24 15:32:17 +0200

    add Films

--- client/MainView.js
+++ client/MainView.js
@@ -1,14 +1,18 @@
 const m = require('mithril');
 
-const model = require('./model');
+// const model = require('./model');
 
 const People = require('./People');
+const Films = require('./Films');
 
 function render() {
   return m('div', [
     m('h1', 'Hello World !!'),
     m('div',
-         m(People),
+         [
+             m(People),
+             m(Films),
+         ]
     ),
   ]);
 }
